e3ad6457f97cf5a88b0803ad5a9d5afafb748cff
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-24 11:25:45 -0500

    updates theme button styling and effect

--- src/components/themeSelector/index.tsx
+++ src/components/themeSelector/index.tsx
@@ -1,22 +1,29 @@
 // src/components/ThemeSelector.tsx
 import React, { useContext } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
+import "./themeSelector.css";
 
 const themes = ["travel", "art", "pets"];
 
 const ThemeSelector: React.FC = () => {
-  const { setTheme } = useContext(ThemeContext);
+  const { theme, setTheme } = useContext(ThemeContext);
 
-  const handleThemeChange = (theme: string) => {
-    setTheme(theme);
+  const handleThemeChange = (selectedTheme: string) => {
+    setTheme(selectedTheme);
   };
 
   return (
     <div>
       <h3>Select Theme:</h3>
-      {themes.map((theme) => (
-        <button key={theme} onClick={() => handleThemeChange(theme)}>
-          {theme.charAt(0).toUpperCase() + theme.slice(1)}
+      {themes.map((themeOption) => (
+        <button
+          className={`reset-button ${
+            theme === themeOption ? "selectedTheme" : ""
+          }`}
+          key={themeOption}
+          onClick={() => handleThemeChange(themeOption)}
+        >
+          {themeOption.charAt(0).toUpperCase() + themeOption.slice(1)}{" "}
         </button>
       ))}
     </div>
