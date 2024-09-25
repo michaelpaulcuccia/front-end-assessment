1e4167ca43db286d4ff0892a2aa52757f6527e1a
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-25 08:29:51 -0500

    moves logo from header component to navbar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./header.css";
-import zicasso_logo from "../../assets/zicasso_logo.png";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -9,7 +8,6 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ children }) => {
   return (
     <header className="header">
-      <img className="header-image" src={zicasso_logo} alt="Zicasso Logo" />
       <div>{children}</div>
     </header>
   );
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./navbar.css";
 import Line from "../line";
+import zicasso_logo from "../../assets/zicasso_logo.png";
 
 interface NavBarProps {
   children: React.ReactNode; // Typing for the children prop
@@ -9,6 +10,7 @@ interface NavBarProps {
 const NavBar: React.FC<NavBarProps> = ({ children }) => {
   return (
     <>
+      <img className="nav-image" src={zicasso_logo} alt="Zicasso Logo" />
       <div className="nav-container">{children}</div>
       <Line />
     </>
