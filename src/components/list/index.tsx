db51d68b8e889a2965c374efc3dda622ae10904d
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-25 11:48:34 -0500

    adds about page

---
+++ src/components/list/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./list.css";
+
+interface ListProps {
+  icon: React.ReactElement; // Accepts a React element (e.g., an icon component)
+  text: string | React.ReactElement; // Accepts either a string or a React element
+}
+
+const List: React.FC<ListProps> = ({ icon, text }) => {
+  return (
+    <div className="list-container">
+      <div className="item-container">
+        <div>{icon}</div>
+        <div className="text-container">{text}</div>
+      </div>
+    </div>
+  );
+};
+
+export default List;
--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,4 +1,12 @@
 import { Helmet } from "react-helmet";
+import { LuCookie } from "react-icons/lu";
+import { FiNavigation } from "react-icons/fi";
+import { BiAccessibility } from "react-icons/bi";
+import { FaReact } from "react-icons/fa";
+import { GiPokerHand } from "react-icons/gi";
+import { IoRadioButtonOnSharp } from "react-icons/io5";
+import { TbSeo } from "react-icons/tb";
+import List from "../components/list";
 
 const About = () => {
   return (
@@ -21,51 +29,45 @@ const About = () => {
         the features I worked on:
       </h3>
       <div style={{ marginTop: "24px" }}>
-        <ul className="about-list">
-          <li>
-            <h3>Cookie consent modal for user agreement.</h3>
-          </li>
-          <li>
-            <h3>
-              Animated navigation bar and company logo that appear upon page
-              load.
-            </h3>
-          </li>
-          <li>
-            <h3>
+        <List
+          text="Cookie consent modal for user agreement."
+          icon={<LuCookie fontSize={32} />}
+        />
+        <List
+          text="Animated navigation bar and company logo that appear upon page load."
+          icon={<FiNavigation fontSize={32} />}
+        />
+        <List
+          icon={<BiAccessibility fontSize={32} />}
+          text={
+            <>
               Accessibility tested<sup>1</sup> using the "Tab" key and VoiceOver
               on Safari.
-            </h3>
-          </li>
-          <li>
-            <h3>
-              Uses React Context to manage the game's card faces. App is set by
-              default to travel, a reflection of Zicasso.
-            </h3>
-          </li>
-          <li>
-            <h3>
-              Displays playing cards in a responsive grid with animations for
-              flipping. Includes a modal to show match results and winning
-              notifications.
-            </h3>
-          </li>
-          <li>
-            <h3>Provides a Reset button to "shuffle deck".</h3>
-          </li>
-          <li>
-            <h3>Implements React Helmet for improved SEO.</h3>
-          </li>
-          <li>
-            <h3></h3>
-          </li>
-        </ul>
-        <div style={{ marginTop: "12px" }}>
-          <small>
-            1. Accessibility testing for the game component has not been
-            conducted.{" "}
-          </small>
-        </div>
+            </>
+          }
+        />
+        <List
+          icon={<FaReact fontSize={32} />}
+          text="Uses React Context to manage the game's card faces. App is set by default to travel, a reflection of Zicasso."
+        />
+        <List
+          icon={<GiPokerHand fontSize={32} />}
+          text="Displays playing cards in a responsive grid with animations for flipping. Includes a modal to show match results and winning notifications."
+        />
+        <List
+          icon={<IoRadioButtonOnSharp fontSize={32} />}
+          text="Provides a Reset button to shuffle deck."
+        />
+        <List
+          text="Implements React Helmet for improved SEO"
+          icon={<TbSeo fontSize={32} />}
+        />
+      </div>
+      <div style={{ marginTop: "12px" }}>
+        <small>
+          1. Accessibility testing for the game component has not been
+          conducted.{" "}
+        </small>
       </div>
     </>
   );
