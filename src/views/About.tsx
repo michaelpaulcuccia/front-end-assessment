91753f5ecae2fce5f6bf8e2bd77f21b81376cd03
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-09-25 11:19:03 -0500

    adds about details

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -20,9 +20,53 @@ const About = () => {
         Thank you for the opportunity to work on this project! Here are some of
         the features I worked on:
       </h3>
-      <ul>
-        <li></li>
-      </ul>
+      <div style={{ marginTop: "24px" }}>
+        <ul className="about-list">
+          <li>
+            <h3>Cookie consent modal for user agreement.</h3>
+          </li>
+          <li>
+            <h3>
+              Animated navigation bar and company logo that appear upon page
+              load.
+            </h3>
+          </li>
+          <li>
+            <h3>
+              Accessibility tested<sup>1</sup> using the "Tab" key and VoiceOver
+              on Safari.
+            </h3>
+          </li>
+          <li>
+            <h3>
+              Uses React Context to manage the game's card faces. App is set by
+              default to travel, a reflection of Zicasso.
+            </h3>
+          </li>
+          <li>
+            <h3>
+              Displays playing cards in a responsive grid with animations for
+              flipping. Includes a modal to show match results and winning
+              notifications.
+            </h3>
+          </li>
+          <li>
+            <h3>Provides a Reset button to "shuffle deck".</h3>
+          </li>
+          <li>
+            <h3>Implements React Helmet for improved SEO.</h3>
+          </li>
+          <li>
+            <h3></h3>
+          </li>
+        </ul>
+        <div style={{ marginTop: "12px" }}>
+          <small>
+            1. Accessibility testing for the game component has not been
+            conducted.{" "}
+          </small>
+        </div>
+      </div>
     </>
   );
 };
